28adffed998d26620dc1aac6e776f3fca3ea31f9
Anthony Fadly <18fadly.anthony@gmail.com>
2017-09-20 14:41:37 -0700

    now has offers

--- 02js/nv.js
+++ 02js/nv.js
@@ -1,3 +1,14 @@
+//functions for writing html from javascript
+function h1(a) {
+    document.write("<h1>" + a + "</h1>");
+}
+function h2(a) {
+    document.write("<h2>" + a + "</h2>");
+}
+function a(text, url) {
+    document.write("<a href = " + url + ">" + text + "</a>");
+}
+
 var p, q, t; //price, quantity, total
 p = 5; //$5 per title
 q = 14; // 14 titles for "montague house"
@@ -12,8 +23,34 @@ function calc() {
     var element = document.getElementById('cost');
     element.textContent = 'Cost: $' + t;
 }
+
+var name;
+
 function hello() {
-    var name = document.getElementById('name').value;
+    name = document.getElementById('name').value;
     var element = document.getElementById('nameplace');
     element.textContent = 'Hello ' + name;
+}
+
+//name = "bob"; //TODO not hardcode
+//var message = "<a href=\'sale.html'\>25% off~!</a>" //"wow"
+
+var offers = [10, 25, 50, 75, 100];
+var names = ["Jenny", "bob", "John", 'Max'];
+function offr() {
+    hello();
+    var i, listed;
+    for (i = 0; i < names.length; i++) {
+        listed = names[i];
+        if (listed == name) {
+            console.log("match")
+            var match = true
+            var offrnm = Math.floor(Math.random()*offers.length);
+            console.log(offrnm)
+            console.log(offers[offrnm]);
+        }
+    }
+    if (match != true) {
+        console.log("new person");
+    }
 }
