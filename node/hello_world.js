0926658710c2e26eaac7e0b6ef7f31572d8f59d2
Anthony Fadly <18fadly.anthony@gmail.com>
2017-09-22 14:21:09 -0700

    calculate area

--- node/hello_world.js
+++ node/hello_world.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+function calcArea(width, height){
+    var area = width*height
+    return area;
+}
+
+var wall1 = calcArea(3,5);
+var wall2 = calcArea(8,5);
+
+console.log(wall1, "Wall1")
+console.log(wall2, "Wall2")
