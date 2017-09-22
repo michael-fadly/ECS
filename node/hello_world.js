ecdb1e6fc64b6d96eb30e5583ec8986b620b63bb
Anthony Fadly <18fadly.anthony@gmail.com>
2017-09-22 14:32:06 -0700

    more functions

--- node/hello_world.js
+++ node/hello_world.js
@@ -1,6 +1,6 @@
 console.log("Hello World!");
-
-function calcArea(width, height){
+/*
+function calcArea(width, height){ //this function can be used outside of this script
     var area = width*height
     return area;
 }
@@ -10,3 +10,19 @@ var wall2 = calcArea(8,5);
 
 console.log(wall1, "Wall1")
 console.log(wall2, "Wall2")
+
+var areaX = function(w,h){ //this function can not
+    return w*h;
+}
+
+var size = areaX(3,5);
+console.log(size);
+*/
+//self invoke
+(function(){
+    var w,h
+    w=3;
+    h=2;
+    console.log(w*h);
+    return(w*h);
+}());
