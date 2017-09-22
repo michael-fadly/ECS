ee0d3f2570155a64c791f6445e1f85e034d47090
Anthony Fadly <18fadly.anthony@gmail.com>
2017-09-22 13:46:01 -0700

    message span and js

--- 02js/bool_var.js
+++ 02js/bool_var.js
@@ -1,1 +1,10 @@
-//yes
+//mkae vars
+var instck, shpng;
+instck = true;
+shpng = false;
+
+//git stock 
+var elstck = document.getElementById("stock");
+
+//something class
+elstck.className = instck;
