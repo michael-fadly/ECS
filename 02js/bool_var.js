d477fca6d3c7bc07e5dd53292d7af8cf70f7bcc5
Anthony Fadly <18fadly.anthony@gmail.com>
2017-09-22 13:58:39 -0700

    the display works

--- 02js/bool_var.js
+++ 02js/bool_var.js
@@ -1,10 +1,13 @@
 //mkae vars
 var instck, shpng;
-instck = true;
-shpng = false;
+instck = false;
+shpng = true;
 
 //git stock 
 var elstck = document.getElementById("stock");
 
 //something class
 elstck.className = instck;
+
+var elshp = document.getElementById("shipping");
+elshp.className = shpng;
