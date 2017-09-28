6ff7424ffc18a898a6fe8f51e71786fab732d87f
Anthony Fadly <18fadly.anthony@gmail.com>
2017-09-28 14:15:06 -0700

    for loop circles

--- 03/repeat.js
+++ 03/repeat.js
@@ -5,9 +5,24 @@ function setup() {
 }
 
 function draw() {
+    //this will draw a bunch of circles
+    var a = 50;
+    var b = 50;
+    var c = 80;
+    var d = 80;
+    //maximum amount of line
+    var limit = 6;
+    
+    //amount to shift in x
+    var increase = 20;
+    fill(100,100,100,100);
+    
+    for (var i=0; i<limit; i++) {
+        ellipse(a+(i*increase*2),b+(i*increase*2),c+(i*increase),d+(i*increase));
+    }
+    /*
     //this will draw a bunch of lines
     
-    //position of first line
     var a = 20;
     var b = 40;
     var c = 80;
@@ -23,4 +38,5 @@ function draw() {
     for (var i=0; i<limit; i++) {
         line(a+(i*increase),b,c+(i*increase),d);
     }
+    */
 }
