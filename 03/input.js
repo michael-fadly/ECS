72ccd20b12bbf6428ee4e3800c330d9ca4e8f574
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-02 13:32:38 -0700

    works a little

--- 03/input.js
+++ 03/input.js
@@ -1,7 +1,8 @@
-var textfield, output;
+var textfield, output, submit;
 
-function setup(){
-    textfield=select("#storyfield");
-    var value = textfield.value();
-    console.log(value);
+function setup() {
+    textfield = select("#storyfield");
+    //var value = textfield.value();
+    console.log('test');
+
 }
