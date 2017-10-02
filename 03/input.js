9295140d3cdda970e7d54004e9315346f92ab5d2
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-02 14:31:31 -0700

    twitter

--- 03/input.js
+++ 03/input.js
@@ -3,7 +3,7 @@ var textfield, output, submit;
 function setup() {
     textfield = select("#storyfield");
     submit = select("#submit");
-    submit.mousePressed(newText);
+    submit.mousePressed(textLimiter); //newText);
     console.log('test');
     console.log(textfield.value());
     output = select('#output');
@@ -11,11 +11,22 @@ function setup() {
 }
 
 function newText(){
-    console.log('yams');
+    console.log(textfield.value());
     createP(textfield.value());
 }
 
 function newTyping(){
-    console.log(textfield.value());
+    //console.log(textfield.value());
     output.html(textfield.value());
+}
+
+function textLimiter(){
+    var text = textfield.value();
+    console.log(text.length + ' chars');
+    if (text.length > 140){
+        createP('over 140 chars')
+    }else{
+        //createP(textfield.value());
+        newText();
+    }
 }
