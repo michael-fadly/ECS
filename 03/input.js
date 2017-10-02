7f5999afbc98c3b8ad1818c70e29e1141cfd089b
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-02 14:17:32 -0700

    chat

--- 03/input.js
+++ 03/input.js
@@ -6,9 +6,16 @@ function setup() {
     submit.mousePressed(newText);
     console.log('test');
     console.log(textfield.value());
+    output = select('#output');
+    textfield.input(newTyping)
 }
 
 function newText(){
     console.log('yams');
     createP(textfield.value());
+}
+
+function newTyping(){
+    console.log(textfield.value());
+    output.html(textfield.value());
 }
