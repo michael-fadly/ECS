78bc1227bee9a73ef1a3acc8844cb5b3f31257d2
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-04 11:01:28 -0700

    read file (doesn't work yet)

--- 03/file.js
+++ 03/file.js
@@ -1,9 +1,10 @@
 var txt;
 
-function preload(){
-    
+function preload(){ // setup will not run until preload is done
+    txt = loadStrings("sample.txt");
 }
 
 function setup(){
     noCanvas(); //specify don't draw anything
+    console.log(txt);
 }
