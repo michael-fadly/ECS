7935c6273f1616fbe5e0e7d3ec1f6cb22e1f0620
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-04 11:10:04 -0700

    still doesn't work

--- 03/file.js
+++ 03/file.js
@@ -1,10 +1,13 @@
 var txt;
 
 function preload(){ // setup will not run until preload is done
-    txt = loadStrings("sample.txt");
+    txt = loadStrings('sample.txt', loaded);
 }
 
 function setup(){
     noCanvas(); //specify don't draw anything
+}
+
+function loaded(){
     console.log(txt);
 }
