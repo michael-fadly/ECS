5843cf9c8be52a7fb90279edf936549311897337
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-06 12:47:44 -0700

    use createfileinput

--- 03/file.js
+++ 03/file.js
@@ -1,16 +1,23 @@
 var txt;
 
+/*
 function preload(){ // setup will not run until preload is done
     txt = loadStrings('sample.txt');
 }
+*/
 
 function setup(){
     noCanvas(); //specify don't draw anything
+    createFileInput();/*
     console.log(txt);
-    createP(txt);
+    //createP(txt);
+    createP(join(txt,'<br/>'));
+    */
 }
+
 /*
 function loaded(){
     console.log(txt);
 }
 */
+
