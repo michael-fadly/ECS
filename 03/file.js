22865da63a6654b341b4ede127a0edac16f5ede0
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-10 14:30:13 -0700

    dropzone works

--- 03/file.js
+++ 03/file.js
@@ -13,15 +13,18 @@ function setup(){
     console.log(txt);
     //createP(txt);
     createP(join(txt,'<br/>'));
-    */
+    
     dropzone = select('#dropzone');
     dropzone.dragOver(highlight);
     dropzone.dragLeave(unhighlight);
     dropzone.drop(gotFile,unhighlight);
-    
+    */
     var canvas = createCanvas(200,200);
     background(200);
     canvas.dragOver(highlight);
+    canvas.dragLeave(unhighlight);
+    canvas.drop(gotFile,unhighlight);
+    
 }
 function highlight(){
     dropzone.style('background-color','#000000')
@@ -33,6 +36,8 @@ function gotFile(file){
     createP(file.name);
     createP(file.type);
     createP(file.size);
+    var img = createImg(file.data);
+    image(img,0,0,200,200);
 }
 function fileSelected(file){
 	console.log(file);
