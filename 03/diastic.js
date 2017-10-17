e771d2f0eda6245011167ea2119aed92dc781ab6
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-17 13:43:45 -0700

    cleanup

--- 03/diastic.js
+++ 03/diastic.js
@@ -1,32 +1,32 @@
 var srctxt, words, submit, seed;
-function preload(){
-    srctxt = loadStrings('sample.txt');//'https://u.teknik.io/dflXn.txt');
+
+function preload() {
+    srctxt = loadStrings('sample.txt');
 }
-function setup(){
+
+function setup() {
     noCanvas();
-    srctxt = join(srctxt,' ');
-    
+    srctxt = join(srctxt, ' ');
+
     words = splitTokens(srctxt, ' .,!?')
 
     seed = select("#seed");
     submit = select("#submit");
-    submit.mousePressed(function(){
+    submit.mousePressed(function () {
         var poem = diastic(seed.value(), words);
     });
 }
-function diastic(se,wo){
+
+function diastic(se, wo) {
     var phrase = "";
-    
+
     for (var j = 0; j < seed.value().length; j++) {
-    var c = seed.value().charAt(j);
-    for (var i = 0; i < words.length; i++) {
-        if (words[i].charAt(j)==seed.value().charAt(j)){
-            createP(words[i]);
-            break;
+        var c = seed.value().charAt(j);
+        for (var i = 0; i < words.length; i++) {
+            if (words[i].charAt(j) == seed.value().charAt(j)) {
+                createP(words[i]);
+                break;
+            }
         }
-    /*  for (var q = 0; q < words[i].length; q++) {
-        if (words[i].charAt(q)==seed.value().charAt(0)){
-            createP(words[i]);}} */
-    }
     }
 }
