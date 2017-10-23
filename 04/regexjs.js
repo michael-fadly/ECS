45d3ff61ec3b22d1427ad69d2212053bddd9eda0
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-23 13:07:43 -0700

    cleanup

--- 04/regexjs.js
+++ 04/regexjs.js
@@ -1,13 +1,3 @@
-/*
-var x = "string"; // string
-
-var y = /string/; // regular expression
-
-x.indexOf(); //index of char
-x.length(); //length of string
-
-*/
-
 var txt, seed, submit;
 
 function setup(){
@@ -16,14 +6,10 @@ function setup(){
     seed = document.getElementById("seed");
     
     submit = document.getElementById("submit");
-    //submit.mousePressed(test);
 }
 
 function test(){
-    //console.log("test");
     var s = document.getElementById("seed").value;
     var regex = /\d{3}[-.]\d{4}/g;
-    //document.write(regex.test(s));
-    //document.getElementById("regtest").innerHTML=regex.test(s);
     document.getElementById("regtest").innerHTML=s.match(regex);
 }
