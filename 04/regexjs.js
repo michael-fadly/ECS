bb5fa05bd79b071237cd0ddd5d8deddc471ebd26
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-25 14:01:36 -0700

    rip exec, splitting function instead

--- 04/regexjs.js
+++ 04/regexjs.js
@@ -21,11 +21,21 @@ function TPE(){ // Test for the Presence of an Email address
     document.getElementById("regtest").innerHTML=s.match(regex);
 }
 
+/*
 function exec(){
     var s = document.getElementById("seed").value;
     var regex = /\d{3}[-.]\d{4}/g;
     var result = regex.exec(s);
     while (result != null){
-        document.getElementById("regtest").innerHTML=result;
+        //document.getElementById("regtest").innerHTML=result[1];
+        console.log(result);
     }
+}
+*/
+
+function splitting(){
+    var s = document.getElementById("seed").value;
+    var regex = /[,.!?\s]+/;
+    var words = s.split(regex);
+    console.log(words);
 }
