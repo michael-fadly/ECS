8cbdaf04c8e6dfc5bfc07fca1ccb683f0f8f60de
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-25 13:54:09 -0700

    exec funcion (idk what it does)

--- 04/regexjs.js
+++ 04/regexjs.js
@@ -20,3 +20,12 @@ function TPE(){ // Test for the Presence of an Email address
     var regex = /\w+[._]?\w+@\w+.(net|com|org)/g;
     document.getElementById("regtest").innerHTML=s.match(regex);
 }
+
+function exec(){
+    var s = document.getElementById("seed").value;
+    var regex = /\d{3}[-.]\d{4}/g;
+    var result = regex.exec(s);
+    while (result != null){
+        document.getElementById("regtest").innerHTML=result;
+    }
+}
