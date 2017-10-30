f9e202141d71883c0cd3f05496efc6bcd2a78c37
Anthony Fadly <18fadly.anthony@gmail.com>
2017-10-30 14:33:07 -0700

    care hotel does not work

--- 05/object-construct.js
+++ 05/object-construct.js
@@ -11,8 +11,20 @@ hotel.checkAvail = function(){
 hotel.name = "Park";
 hotel.booked =30;
 
+var hotel_yeet(name, rooms, booked){
+    this.name=name;
+    this.rooms = rooms;
+    this.booked =booked;
+    
+    hotel.checkAvail = function(){
+        return this.rooms-this.booked;
+    };
+}
+
+var careHotel = new hotel_yeet("Care",60,13);
+
 var elName = document.getElementById("hotelName");
-elName.textContent = hotel.name;
+elName.textContent = carehotel.name;
 
 var elRoom = document.getElementById("rooms");
-elRoom.textContent = hotel.checkAvail();
+elRoom.textContent = carehotel.checkAvail();
