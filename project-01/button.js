134350b746c66bbb7da58da93c7debfc4baf4b84
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-02 20:47:51 -0700

    use number.value

--- project-01/button.js
+++ project-01/button.js
@@ -9,12 +9,15 @@ function press(){
 var number ={
 	value: 0,
 	presss: function(){
-		var q = document.getElementById("q");
+		//var q = document.getElementById("q");
 		var rnum=Math.floor(Math.random()*100)
-		q.textContent=rnum;
+		//q.textContent=rnum;
+		number.value=rnum
 	}
 };
 
 function press(){
+	var q = document.getElementById("q");
 	var w = number.presss();
+	q.textContent=number.value;
 }
