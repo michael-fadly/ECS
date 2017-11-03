7733992e2ab69ce9c56b5f5b73754bf88f22e76c
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-02 20:37:44 -0700

    as object

--- project-01/button.js
+++ project-01/button.js
@@ -1,5 +1,20 @@
+/*
 function press(){
 	var q = document.getElementById("q");
 	var rnum=Math.floor(Math.random()*100)
 	q.textContent=rnum;
+}
+*/
+
+var number ={
+	value: 0,
+	presss: function(){
+		var q = document.getElementById("q");
+		var rnum=Math.floor(Math.random()*100)
+		q.textContent=rnum;
+	}
+};
+
+function press(){
+	var w = number.presss();
 }
