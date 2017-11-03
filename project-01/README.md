2818a0ff919eb806b8e1b998c1032a96b79def52
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-02 21:10:06 -0700

    done

--- project-01/README.md
+++ project-01/README.md
@@ -3,10 +3,11 @@
 ## Objectives
 
 * Create a webpage with a button that when clicked returns a random number
-between 1 and 100
+between 1 and 100 - done
 
-* Put that number in an object
+* Put that number in an object - done?
 
 * Call on the object to check if it's less or greater than 50 you pass or fail
+-done
 
-* HTML: Button, Background Color, Text Changes
+* HTML: Button, Background Color, Text Changes -done
--- project-01/button.js
+++ project-01/button.js
@@ -14,7 +14,7 @@ var number ={
 		var rnum=Math.floor(Math.random()*100);
 		//q.textContent=rnum;
 		number.value=rnum;
-		if (rnum => 50){
+		if (rnum > 49){
 			number.result = true;
 		}
 	}
@@ -22,9 +22,12 @@ var number ={
 
 function press(){
 	var q = document.getElementById("q");
+	var result = document.getElementById("result");
 	var w = number.presss();
 	q.textContent=number.value;
 	if (number.result){
-		console.log("passed");
+		result.textContent="You have passed";
+	}else{
+		result.textContent="You have failed";
 	}
 }
