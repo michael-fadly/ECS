1563051a6f4391f9bc77d6e371990f66fb31e330
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-07 14:03:27 -0800

    .

---
+++ 06/events.js
@@ -0,0 +1,12 @@
+/*
+ * if password is less than x chars(
+ * prevent signup
+ * highlight pswd box in red
+ * make a popup
+ * )
+*/
+
+function chkusr(){
+	var lmsg = document.getElementById("feedback");
+	var lusr = document.getElementById("username");
+}
