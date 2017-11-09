742a4d13b4bc145ed3a10bf864a68febc8e80db9
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-09 12:27:16 -0800

    .

--- 06/events.js
+++ 06/events.js
@@ -23,8 +23,10 @@ function chkusr(minln){
 
 var msg = '<div class=\"header\"><a id=\"close"\ X<\a></div>';
 msg += '<div><h2>System Maintenance</h2>';
-msg += 'Our servers are being updated between 2 and 4am';
+msg += 'Our servers are being updated between 2 and 4am ';
 msg += 'During this time there may be many disruptions to service.</div>';
 
 var elNote = document.createElement('div'); //create an element
 elNote.setAttribute('id','note'); //add id = note
+elNote.innerHTML = msg; //add msg to elnote
+document.body.appendChild(elNote); //add it to the page through the body
