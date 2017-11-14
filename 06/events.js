852ef4bb4c5ea63acea8ff57b7af18d99c1ffe44
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-14 14:10:39 -0800

    all the varibles

--- 06/events.js
+++ 06/events.js
@@ -49,11 +49,15 @@ var sy = document.getElementById("sy");
 var px = document.getElementById("px");
 var py = document.getElementById("py");
 var cx = document.getElementById("cx");
-var cy = document.getElementById("py");
+var cy = document.getElementById("cy");
 
 function showPosition(event){
     sx.value = event.screenX;
     sy.value = event.screenY;
+    px.value = event.pageX;
+    py.value = event.pageY;
+    cx.value = event.clientX;
+    cy.value = event.clientY;
 }
 
 var elStats = document.getElementById('body');
