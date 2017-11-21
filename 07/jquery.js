fa49911d9b0553127707ed96415a3c05fdf12423
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-21 12:11:27 -0800

    kinda works

--- 07/jquery.js
+++ 07/jquery.js
@@ -1,3 +1,4 @@
+/*
 //$(document).read(function(){});
 $(':header').addClass('headline');
 $('li:lt(4)').hide().fadeIn(1500);
@@ -9,15 +10,24 @@ $('li').on('click',function(){
 $('li em').addClass('seasonal');
 //$('li.hot').addClass('favorite');
 
-/*
+
 // chaining
 $('li[id!="one"]').hide().delay.(500).fadeIn(1000);
 $('li:first-child').addClass('next');
 $('li.priority').addClass('highlight');
 */
 
-//get html frag
+//get html frag + node
 $(function () { // self calling function
-    var $listHTML = $('ul').html;
+    var $listHTML = $('ul').html();
     $('ul').append($listHTML);
+
+    var $listItemHTML = $('li').html();
+    $('li').append('<i>'+$listItemHTML+'</i>');
+});
+
+//get text frag
+$(function () { // self calling function
+    var $listText = $('ul').text();
+    $('ul').append('<p>'+$listText+'</p>');
 });
