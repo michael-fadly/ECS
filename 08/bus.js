cbd284c3fcb3577d95279f76188b927f88207d10
Anthony Fadly <18fadly.anthony@gmail.com>
2017-11-27 14:33:19 -0800

    .

--- 08/bus.js
+++ 08/bus.js
@@ -4,9 +4,15 @@ var xhr = new XMLHttpRequest();
 
 xhr.onload = function(){
     //if(xhr.status === 200){
-    if(true){ //not on a server so the other if doesn't make sense
-        document.getElementById('content').innerHTML - xhr.responseText;
+    responseObject - JSON.parse(xhr.responseText);
+    //build
+    var newContent ='';
+    for (var i = 0; i < responseObject.events.length; i++){
+        
     }
+    //if(true){ //not on a server so the other if doesn't make sense
+      //  document.getElementById('content').innerHTML - xhr.responseText;
+    //}
 }
 
 xhr.open('GET', 'date/data.html', true)
