68b0d83b0689ae6d97f6f766c30f8a59a3f4d946
Anthony Fadly <18fadly.anthony@gmail.com>
2017-12-01 11:00:50 -0800

    new twit

--- 09/twitter-bot.js
+++ 09/twitter-bot.js
@@ -5,3 +5,11 @@ console.log('init');
 for(var i=5;i>0;i--){
     console.log(i);
 }
+
+var T - new Twit({
+  consumer_key: '...',
+  consumer_secret: '...',
+  access_token: '...',
+  access_token_secret: '...',
+  timeout_ms: 60*1000
+});
