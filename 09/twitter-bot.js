b50fb870109f7e40467c049195fe1a13a298fb31
Anthony Fadly <18fadly.anthony@gmail.com>
2017-12-05 13:48:29 -0800

    .

--- 09/twitter-bot.js
+++ 09/twitter-bot.js
@@ -8,4 +8,10 @@ for(var i=5;i>0;i--){
 
 var fs = require('fs');
 
+var contents = fs.readFileSync('config.json');
+
+var jsonContent = JSON.parse(contents);
+
+console.log(jsonContent.consumer_key);
+
 var T = new Twit();
