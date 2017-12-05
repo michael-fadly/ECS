6fdfc20f39d9c7fe4410104d47ef7167e93ee937
Anthony Fadly <18fadly.anthony@gmail.com>
2017-12-05 14:17:00 -0800

    server

--- 09/twitter-bot.js
+++ 09/twitter-bot.js
@@ -1,5 +1,6 @@
 var Twit = require('twit');
 var fs = require('fs');
+var express = requires('express');
 
 console.log('init');
 
@@ -10,3 +11,17 @@ var jsonContent = JSON.parse(contents);
 console.log(jsonContent.consumer_key);
 
 //var T = new Twit();
+
+var app = express();
+
+function listening(){
+  console.log("listening");
+}
+
+var server = app.listen(3000, listening);
+
+function sendPikachu(request,response{
+  response.send("Pika");
+}
+
+app.get('/pikachu', sendPikachu);
