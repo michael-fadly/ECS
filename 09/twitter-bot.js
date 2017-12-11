06063f63d7b020031eba40114b63d6b3baa982c7
Anthony Fadly <18fadly.anthony@gmail.com>
2017-12-11 12:47:12 -0800

    it works

--- 09/twitter-bot.js
+++ 09/twitter-bot.js
@@ -1,23 +1,45 @@
 var Twit = require('twit');
 //var fs = require('fs');
 var express = require('express');
-var config = require("./config.js")
+
+var app = express();
+
+function listening() {
+    console.log("listening");
+}
+
+var server = app.listen(3000, listening);
+
+var config = require("./config.js");
 console.log('init');
 var Twitter = new Twit(config);
-var retweet = function(){
+var retweet = function () {
     var params = {
         q: "#nodejs, #Nodejs",
         result_type: "recent",
         lang: "en"
     }
-}
 
-Twitter.get('search/tweets',params,function(err,data){
-   if(!err){
-       
-   } 
+
+Twitter.get('search/tweets', params, function (err, data) {
+    if (!err) {
+        var retweetID = data.statuses[0].id_str;
+        Twitter.post('statuses/retweet/:id', {
+            id: retweetID
+        }, function (err, response) {
+            if (response) {
+                console.log("retweeted");
+            }
+            if (err) {
+                console.log('error while retweeting');
+            }
+        });
+        
+    }else{
+         console.log('error while searching');
+        }
 });
-
+}
 //Twitter.post('statuses/update',{status: 'beep boop'}, function(err, data, response){
 //    console.log(data)
 //})
@@ -29,26 +51,20 @@ Twitter.get('search/tweets',params,function(err,data){
 //console.log(jsonContent.consumer_key);
 
 //var T = new Twit();
-
-var app = express();
-
-function listening(){
-  console.log("listening");
-}
-
-var server = app.listen(3000, listening);
-
-function sendPikachu(request,response){
-  response.send("Pika");
+/*
+function sendPikachu(request, response) {
+    response.send("Pika");
 }
 
 app.get('/pikachu', sendPikachu);
 
-function sendPokemon(request,response){
-  var data = request.params;
-  var reply = "pikachu is the best";
-  console.log(data);
-  response.send(reply);
+function sendPokemon(request, response) {
+    var data = request.params;
+    var reply = "pikachu is the best";
+    console.log(data);
+    response.send(reply);
 }
 
-app.get('/search/pikachu/25',sendPokemon);
+app.get('/search/pikachu/25', sendPokemon);
+*/
+retweet();
