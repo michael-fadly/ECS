cfa4d486c3383a8b5bf1c90806e49a28a2f33507
Anthony Fadly <18fadly.anthony@gmail.com>
2017-12-13 14:22:48 -0800

    lotta stuff and it still doesn't work

--- 09/twitter-bot.js
+++ 09/twitter-bot.js
@@ -7,12 +7,13 @@ var app = express();
 function listening() {
     console.log("listening");
 }
-
+var today = new Date();
 var server = app.listen(3000, listening);
 
 var config = require("./config.js");
 console.log('init');
 var Twitter = new Twit(config);
+/* // uncomment when ready to retweet hashtags
 var retweet = function () {
     var params = {
         q: "artificalintelligence", //"#nodejs, #Nodejs",
@@ -39,17 +40,42 @@ var retweet = function () {
             console.log('error while searching');
         }
     });
-}
-var today = new Date();
+}*/
+/* //uncomment when ready to tweet
 Twitter.post('statuses/update', {
     status: "It's "+today+" and AI hasn't turned evil yet!!!"
 }, function (err, data, response) {
     console.log("tweeted")
 });
+*/
 
 //retweet from LeonMuss4Earth
+var prml = {
+	//screen_name: 'LeonMuss4Earth',
+	screen_name: 'LeonMuss4Earth'
+		   }
+Twitter.get('statuses/user_timeline', prml, function (err, data) {
+        if (!err) {
+			console.log(data);
+            var retweetID = data[0].id_str;/*
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetID
+            }, function (err, response) {
+                if (response) {
+                    console.log("retweeted");
+                }
+                if (err) {
+                    console.log('error while retweeting');
+                }
+            });
+
+        } else {
+            console.log('error while searching');*/
+        }
+    });
+    /*
 Twitter.post('statuses/retweet/:screen_name', {
-               screen_name: 'LeonMuss4Earth'
+               
             }, function (err, response) {
                 if (response) {
                     console.log("retweeted from leon");
@@ -66,7 +92,7 @@ Twitter.post('statuses/retweet/:screen_name', {
 //console.log(jsonContent.consumer_key);
 
 //var T = new Twit();
-/*
+
 function sendPikachu(request, response) {
     response.send("Pika");
 }
